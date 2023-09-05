001e972b0a9bf3ffe3aef49657a6deb2cf24b810
RitaChang5527 <109656848+RitaChang5527@users.noreply.github.com>
2023-09-05 11:10:04 +0800

    Add files via upload

--- data/data_db.py
+++ data/data_db.py
@@ -7,7 +7,7 @@ try:
         host="127.0.0.1",
         port=3306,
         user="root",
-        password="#Abc123456789",
+        password="123456789",
         database="taipei",
     )
 
@@ -19,83 +19,75 @@ try:
         data = json.load(json_file)
         results = data['result']['results']
 
+    # Create a dictionary to consolidate images by attraction name
+    image_dict = {}
+
     # 遍历 JSON 数据并提取图像链接
     for spot in results:
-        # 获取 "file" 字段的内容
-        file_content = spot.get('file', '')
+        attraction_name = spot.get('name', '')
+        images = spot.get('images', [])
 
-        # 使用 "https://" 作为分隔符来切分字符串
-        url_list = file_content.split('https://')
+        if attraction_name not in image_dict:
+            image_dict[attraction_name] = images
+        else:
+            image_dict[attraction_name].extend(images)
 
-        # 遍历切分后的 URL 列表
-        for url in url_list:
-            # 如果 URL 以 ".jpg"、".jpeg" 或 ".png" 结尾，将其添加上 "https://" 前缀
-            if url.lower().endswith((".jpg", ".jpeg", ".png")):
-                image_url = 'https://' + url
-                cursor = db_connection.cursor()
+    # 遍历图像字典并插入数据到数据库
+    cursor = db_connection.cursor()
+    for attraction_name, images in image_dict.items():
+        try:
+            # 检查景点是否已存在
+            check_query = "SELECT id FROM attractions WHERE name = %s"
+            cursor.execute(check_query, (attraction_name,))
+            existing_attraction = cursor.fetchone()
 
-                try:
-                    # 检查景点是否已存在
-                    check_query = "SELECT id FROM attractions WHERE name = %s"
-                    cursor.execute(check_query, (spot.get('name', ''),))
-                    existing_attraction = cursor.fetchone()
+            if existing_attraction:
+                # 如果景点已存在，使用现有的 Attraction ID
+                attraction_id = existing_attraction[0]
+            else:
+                # 插入景点数据到 attractions 表
+                insert_query = (
+                    "INSERT INTO attractions (name, category, description, address, transport, mrt, latitude, longitude) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                )
+                data = (
+                    attraction_name,
+                    spot.get('CAT', ''),
+                    spot.get('description', ''),
+                    spot.get('address', ''),
+                    spot.get('direction', ''),
+                    spot.get('MRT', ''),
+                    spot.get('latitude', ''),
+                    spot.get('longitude', '')
+                )
+                cursor.execute(insert_query, data)
 
-                    if existing_attraction:
-                        # 如果景点已存在，使用现有的 Attraction ID
-                        attraction_id = existing_attraction[0]
-                    else:
-                        # 插入景点数据到 attractions 表
-                        insert_query = (
-                            "INSERT INTO attractions (name, category, description, address, transport, mrt, latitude, longitude) "
-                            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                        )
-                        data = (
-                            spot.get('name', ''),
-                            spot.get('CAT', ''),
-                            spot.get('description', ''),
-                            spot.get('address', ''),
-                            spot.get('direction', ''),
-                            spot.get('MRT', ''),
-                            spot.get('latitude', ''),
-                            spot.get('longitude', '')
-                        )
-                        cursor.execute(insert_query, data)
+                # 提取插入的景点的 ID
+                cursor.execute("SELECT LAST_INSERT_ID()")
+                attraction_id = cursor.fetchone()[0]
 
-                        # 提取插入的景点的 ID
-                        cursor.execute("SELECT LAST_INSERT_ID()")
-                        attraction_id = cursor.fetchone()[0]
+            # 插入图片链接到 attraction_images 表
+            for image_url in images:
+                insert_image_query = (
+                    "INSERT INTO attraction_images (attraction_id, image_url) "
+                    "VALUES (%s, %s)"
+                )
 
-                    # 插入图片链接到 attraction_images 表
-                    insert_image_query = (
-                        "INSERT INTO attraction_images (attraction_id, image_url) "
-                        "VALUES (%s, %s)"
-                    )
+                # 插入图片链接到 attraction_images 表，并关联到对应的景点
+                image_data = (attraction_id, image_url)
+                cursor.execute(insert_image_query, image_data)
 
-                    # 插入图片链接到 attraction_images 表，并关联到对应的景点
-                    image_data = (attraction_id, image_url)
-                    cursor.execute(insert_image_query, image_data)
+            # 提交更改并获取图片的 ID
+            db_connection.commit()
+            print("Processed Attraction:", attraction_name)
+            print("Attraction ID:", attraction_id)
 
-                    # 提交更改并获取图片的 ID
-                    db_connection.commit()
-                    image_id = cursor.lastrowid
-
-                    print("Processed Attraction:", spot['name'])
-                    print("Attraction ID:", attraction_id)
-                    print("Image ID:", image_id)
-
-                except mysql.connector.Error as err:
-                    print("MySQL Error:", err)
-
-                finally:
-                    # 关闭游标
-                    cursor.close()
-
-    print("DONE")
-
-except mysql.connector.Error as err:
-    print("数据库连接错误:", err)
+        except mysql.connector.Error as err:
+            print("MySQL Error:", err)
 
 finally:
     # 最后，确保关闭数据库连接
     if db_connection.is_connected():
         db_connection.close()
+
+print("DONE")
