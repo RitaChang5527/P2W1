5725415c94f42cc17fafadef9d446729fafaa313
RitaChang5527 <lynn02110211@gmai.com>
2023-09-26 23:32:40 +0800

    fix booking

--- api/booking_api.py
+++ api/booking_api.py
@@ -12,9 +12,20 @@ db_config = {
 }
 pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="attractions_pool", pool_size=10, **db_config)
 
+jwt_secret_key = "taipei-day-trip"
+
 booking = Blueprint("booking", __name__)
 
 @booking.route("/api/booking", methods=['GET'])
 def get_bookingData():
-        conn = pool.get_connection()
-        cursor = conn.cursor(dictionary=True)
+        # conn = pool.get_connection()
+        # cursor = conn.cursor(dictionary=True)
+
+        auth_header = request.headers.get("Authorization")
+        return auth_header
+        print(auth_header)
+        try:
+            token=auth_header.split(" ")[1]
+            print("token : "+token)
+        except Exception as e:
+            print(e)
